6abfce5d5b2c947a8910ed704d0bbb3005635fbf
Andrei <dinamo1999@icloud.com>
2023-06-26 23:43:12 +0200

    changes

--- frontend/src/components/UserPage.vue
+++ frontend/src/components/UserPage.vue
@@ -21,23 +21,7 @@
                     :items-per-page="10"
                     class="elevation-1"
                 >
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn
-                            color="blue darken-1"
-                            text
-                            @click="close"
-                        >
-                            Cancel
-                        </v-btn>
-                        <v-btn
-                            color="blue darken-1"
-                            text
-                            @click="save"
-                        >
-                            Save
-                        </v-btn>
-                    </v-card-actions>
+                
                 </v-data-table>
             </div>
         </div>
