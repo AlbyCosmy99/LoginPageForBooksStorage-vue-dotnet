bb3e0609fe0bce323a107e1515dd528beec97d6c
Andrei <dinamo1999@icloud.com>
2023-06-27 00:02:03 +0200

    changes

--- frontend/src/components/UserPage.vue
+++ frontend/src/components/UserPage.vue
@@ -21,7 +21,90 @@
                     :items-per-page="10"
                     class="elevation-1"
                 >
-                
+                    <template v-slot:top>
+                        <v-toolbar 
+                            flat
+                        >
+                            <v-toolbar-title>My CRUD</v-toolbar-title>
+                            <v-divider
+                                class="mx-4"
+                                inset
+                                vertical
+                            ></v-divider>
+                            <v-spacer></v-spacer>
+                            <v-dialog
+                                v-model="dialog"
+                                max-width="500px"
+                            >
+                                <template  v-slot:activator="{ on, attrs }">
+                                    <v-btn
+                                        color="primary"
+                                        dark
+                                        class="mb-2"
+                                        v-bind="attrs"
+                                        v-on="on"
+                                    >
+                                    Add book
+                                    </v-btn>
+                                </template>
+                                <v-card>
+                                    <v-card-title>
+                                        <span class="text-h5">{{ formTitle }}</span>
+                                    </v-card-title>
+
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn
+                                            color="blue darken-1"
+                                            text
+                                            @click="close"
+                                        >
+                                            Cancel
+                                        </v-btn>
+                                        <v-btn
+                                            color="blue darken-1"
+                                            text
+                                            @click="save"
+                                        >
+                                            Save
+                                        </v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                            <v-dialog v-model="dialogDelete" max-width="500px">
+                                <v-card>
+                                    <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                                        <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                                        <v-spacer></v-spacer>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                        </v-toolbar>
+                    </template>
+                    <template>
+                        <v-icon
+                        small
+                        class="mr-2"
+                        >
+                            mdi-pencil
+                        </v-icon>
+                        <v-icon
+                            small
+                        >
+                            mdi-delete
+                        </v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                        <v-btn
+                            color="primary"
+                            @click="initialize"
+                        >
+                            Reset
+                        </v-btn>
+                    </template>
                 </v-data-table>
             </div>
         </div>
